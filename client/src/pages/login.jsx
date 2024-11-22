1c38438ccedafb74845ec58401a95fa75713271a
Dhillips19 <daniel.phillips@outlook.ie>
2024-11-22 15:30:06 +0000

    Began work on login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/NavBar';
+
+// const Login = () => {
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//     }
+// }
 
 export default function Login() {
     return (
